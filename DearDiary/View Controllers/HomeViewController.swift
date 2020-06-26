53c4b7e33be2f2ea5eb64c8d1bea27d64e083f12
Ronit Dhingra <rdhingra001@hotmail.com>
2020-06-26 15:54:48 -0400

    initial creation of xcode project

---
+++ DearDiary/View Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DearDiary
+//
+//  Created by Ronit Dhingra on 6/26/20.
+//  Copyright © 2020 Ronit Dhingra. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
