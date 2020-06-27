053b718404edcf0b63d3a6b7fcbe3bc4eba042ed
Ronit Dhingra <rdhingra001@hotmail.com>
2020-06-26 22:47:05 -0400

    added firebase

--- DearDiary/View Controllers/SignUpViewController.swift
+++ DearDiary/View Controllers/SignUpViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpViewController: UIViewController {
     
@@ -50,9 +51,66 @@ class SignUpViewController: UIViewController {
         Utilities.styleFilledButton(signUpButton)
     }
     
-    @IBAction func signUpTapped(_ sender: Any) {
+    // Validate the fields
+    func validateFields() -> String? {
+        
+        // Check that all fields are filled in
+        if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+            return "Please fill in all the fields"
+        }
+        
+        // Check if the password is secure
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if Validation.isPasswordValid(cleanedPassword) == false {
+            // Password isn't secure enough
+            return "Please make sure your password is at least 8 characters, contains a special character, and a number. "
+        }
+        
+        return nil
     }
     
+    @IBAction func signUpTapped(_ sender: Any) {
+        
+        // Validate the fields
+        let error = validateFields()
+        
+        if error != nil {
+            
+            // There's something wrong with the fields; show error message
+            showError(error!)
+        }
+        else {
+            
+            // Create the user
+            Auth.auth().createUser(withEmail: <#T##String#>, password: <#T##String#>) { (result, err) in
+                
+                // Check for errors
+                if err != nil {
+                    
+                    // There was an error creating the user
+                    self.showError("Error creating user")
+                }
+                else {
+                    
+                    // User was created successfully; store credentials
+                    let db = Firestore.firestore()
+                }
+            }
+            
+            // Transition to the setup screen
+        }
+
+    }
+    
+    func showError(_ message:String) {
+        
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
 
     /*
     // MARK: - Navigation
